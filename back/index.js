7f47362648a03faa012f8716fd246473bdbbeee6
Patricia <patricia_steffen99@hotmail.com>
2022-04-15 22:58:57 -0300

    Sqlite3

--- back/index.js
+++ back/index.js
@@ -8,13 +8,12 @@ const routes = require('./routes');
 const Boom = require('boom');
 
 
-const cacheClient = new Catbox.Client(CatboxMemory);
-
 const server = Hapi.server({
   port: process.env.PORT || 5000,
   host: process.env.HOST || 'localhost',
-  routes: { cors: true }
+  routes: { cors: true },
 });
+
 
 const validate = async function (decoded, request) {
   console.log('running validate...');
@@ -30,15 +29,15 @@ const startServer = async () => {
   try {
     await server.register(require('hapi-auth-jwt2'));
 
-    server.auth.strategy('jwt', 'jwt', { 
+    server.auth.strategy('jwt', 'jwt', {
       key: process.env.SECRET_KEY,
       validate,
-      verifyOptions: { 
-        algorithms: [ 'HS256' ] 
+      verifyOptions: {
+        algorithms: ['HS256']
       }
     });
 
-    routes.forEach((route)=>{
+    routes.forEach((route) => {
       server.route(route);
     });
 
